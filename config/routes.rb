661344ba8906ea37314bb1ec890e08caa97a3346
hcurotta <hcurotta@gmail.com>
2012-07-23 16:41:21 -0500

    with tooltips

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ get "/" => "Words#index"
 
 get "/words" => "Words#index", :as => :words
 get "/words/new" => "Words#new", :as => :new_word
+get "/words/flagged" => "Words#flagged", :as => :flagged_words
 
 
 get "/words/:id" => "Words#show", :as => :show_word
 
 
-get "/words/flagged" => "Words#flagged", :as => :flagged_words
 
 post "/words/" => "Words#create", :as => :words
 
